f120709e9c5dc98cbc3206534c43e12e614771f1
liezh <13556024285@163.com>
2018-03-14 11:53:49 +0800

    bug fix TODO  用户模块    编辑页细节

--- src/main/java/com/liezh/controller/FoodnoteController.java
+++ src/main/java/com/liezh/controller/FoodnoteController.java
@@ -12,6 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2018/2/28.
  */
@@ -87,6 +89,7 @@ public class FoodnoteController extends BaseController {
         Long myId = this.getAuthUserId();
         foodnote.setAuthorId(myId);
         foodnote.setStatus(GlobalConstants.STATUS_RELEASE);
+        foodnote.setReleaseTime(new Date());
         if (foodnote.getId() == null) {
             return foodnoteService.insertFoodnote(foodnote);
         }
